da1a07715eca2bda6f93d6df7d54ee688f78bf94
Dawid.Rym <dawid.jscript@gmail.com>
2020-04-24 21:25:50 +0200

    creating interfaces. Close #4

---
+++ pet-clinic-data/src/main/java/com/dwr/petclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package com.dwr.petclinic.services;
+
+import com.dwr.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
