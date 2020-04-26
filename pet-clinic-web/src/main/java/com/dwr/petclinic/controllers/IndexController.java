859c7becd9f37c24decd78185e52849b92a569a6
Dawid.Rym <dawid.jscript@gmail.com>
2020-04-26 20:15:20 +0200

    adding pet clinic index page controller. Close #7

---
+++ pet-clinic-web/src/main/java/com/dwr/petclinic/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package com.dwr.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/index", "index.html"}) //when the request comes in to  the root context or root slash index or index.html the are gonna match to the this RequestMapping
+    public String index(){
+
+        return "index";
+    }
+}
