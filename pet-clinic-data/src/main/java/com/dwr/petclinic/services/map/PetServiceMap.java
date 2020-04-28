e7e97a6cc834fbaeea200b3104c109b97c5678ae
Dawid.Rym <dawid.jscript@gmail.com>
2020-04-28 20:25:03 +0200

    adding bootstrap for initializing data. Close #13

--- pet-clinic-data/src/main/java/com/dwr/petclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/dwr/petclinic/services/map/PetServiceMap.java
@@ -1,11 +1,11 @@
 package com.dwr.petclinic.services.map;
 
 import com.dwr.petclinic.model.Pet;
-import com.dwr.petclinic.services.CrudService;
+import com.dwr.petclinic.services.PetService;
 
 import java.util.Set;
 
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
         return super.findAll();
--- pet-clinic-data/src/main/java/com/dwr/petclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/dwr/petclinic/services/map/VetServiceMap.java
@@ -1,11 +1,11 @@
 package com.dwr.petclinic.services.map;
 
 import com.dwr.petclinic.model.Vet;
-import com.dwr.petclinic.services.CrudService;
+import com.dwr.petclinic.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
