8e5cdc09585bc33e28ee4cc75b858ac3e5968d98
Dawid.Rym <dawid.jscript@gmail.com>
2020-05-09 20:31:20 +0200

    added visitService and visitServiceMap(visitService implementation). Close #44

---
+++ pet-clinic-data/src/main/java/com/dwr/petclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package com.dwr.petclinic.services;
+
+import com.dwr.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/com/dwr/petclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,39 @@
+package com.dwr.petclinic.services.map;
+
+
+import com.dwr.petclinic.model.Visit;
+import com.dwr.petclinic.services.VisitService;
+
+import java.util.Set;
+
+
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit obj) {
+        if(obj.getPet() == null || obj.getPet().getOwner() == null || obj.getPet().getId() == null ||
+        obj.getPet().getOwner().getId() == null){
+            throw new RuntimeException("Invalid visit");
+        }
+        return super.save(obj);
+    }
+
+    @Override
+    public void delete(Visit obj) {
+        super.delete(obj);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
